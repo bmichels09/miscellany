f77367c8c98fb91000b404010a2b48388e5d9b35
Brandon Michels <bmichels09@gmail.com>
2021-10-05 15:50:04 -0500

    Add date filtering to map

--- COVIDStatus/Scripts/getData.js
+++ COVIDStatus/Scripts/getData.js
@@ -1,4 +1,4 @@
-states = [
+var states = [
     {name: "Alabama", abbr: "AL", population: 5024279},
     {name: "Alaska", abbr: "AK", population: 733391},
     {name: "Arizona", abbr: "AZ", population: 7151502},
@@ -50,22 +50,76 @@ states = [
     {name: "Wisconsin", abbr: "WI", population: 5893718},
     {name: "Wyoming", abbr: "WY", population: 576851},
     {name: "District of Columbia", abbr: "DC", population: 689545}
-]
+];
 
-function getDeathCount(dataSeries) {
-    let currentTimestamp = Date.parse(new Date());
-    let filterTimestamp = currentTimestamp - (15 * 86400000);
+// Adjustment factors to estimate the population on July 1st of a given year.
+// 2020 Census data is for 4/1/2020.
+// So adj2019 is the US population on 7/1/2019 divided by the US population on 4/1/2020, and so on for the other years.
+var censusData = {
+    usPop: 331449281,
+    adj2019: 0.9963114357,
+    adj2020: 1.000257599,
+    adj2021: 1.0030968298
+};
+
+function getDeathCount(dataSeries, endTimestamp) {
     let deaths = 0;
+    let nullDays = 0;
+    let startTimestamp = endTimestamp - (14 * 86400000);
+    
+    // Count the deaths for each day that's between 1 and 14 days ago, inclusive
     dataSeries.forEach(data => {
-        let dayTimestamp = Date.parse(data.date);
-        if (dayTimestamp > filterTimestamp) {
+        let dataDate = new Date();
+        dataDate.setFullYear(parseInt(data.date.substring(0, 4)));
+        dataDate.setMonth(parseInt(data.date.substring(5, 7)) - 1);
+        dataDate.setDate(parseInt(data.date.substring(8)));
+        dataDate.setHours(0, 0, 0, 0);
+        let dataTimestamp = dataDate.getTime();
+        
+        if (dataTimestamp >= startTimestamp && dataTimestamp < endTimestamp) {
             let dailyDeaths = data.newDeaths;
             if (dailyDeaths != null) {
                 deaths = deaths + dailyDeaths;
             }
+            else {
+                // Count the number of null days to display in the map tooltip
+                nullDays = nullDays + 1;
+            }
         }
     })
-    return deaths;
+
+    return {
+        deaths: deaths,
+        nullDays: nullDays
+    };
+}
+
+function getEndTimestamp() {
+    // Get the date parameter from the query string
+    let qsParams = new URLSearchParams(window.location.search);
+    let dateParam = qsParams.get("date");
+    let endDate = new Date();
+
+    console.log("dateParam = " + dateParam);
+
+    // If the date parameter has a value, try to get a valid date from it
+    if (dateParam !== undefined) {
+        try {
+            endDate.setFullYear(parseInt(dateParam.substring(0, 4)));
+            endDate.setMonth(parseInt(dateParam.substring(4, 6)) - 1);
+            endDate.setDate(parseInt(dateParam.substring(6)));
+        }
+
+        catch {
+            // If the parameter specifies an invalid date, just use the current date
+            endDate = new Date();
+        }
+    }
+
+    // Set the time to midnight so it will match with dates in the Covid data
+    endDate.setHours(0, 0, 0, 0);
+
+    return endDate.getTime();
 }
 
 function getColor(ddpm) {
@@ -75,9 +129,9 @@ function getColor(ddpm) {
     else { return "rgb(224, 64, 64)"; }
 }
 
-function getTooltip(stateName, deaths, ddpm) {
+function getTooltip(stateName, deaths, ddpm, nullDays) {
     return '<h3>'+ stateName + '</h3><hr><h4>Total deaths (last 14 days):&nbsp;' +
-        deaths + '<br>Daily deaths/million:&nbsp;' + ddpm.toFixed(2) + '</h4>';
+        deaths + '<br>Daily deaths/million:&nbsp;' + ddpm.toFixed(2) + '<br>Unrecorded days:&nbsp;' + nullDays + '</h4>';
 }
 
 function setColorAndTooltip(stateAbbr, color, tooltip) {
@@ -99,8 +153,18 @@ function setColorAndTooltip(stateAbbr, color, tooltip) {
 }
 
 function fillMap() {
-    let finalData = [];
-    let errors = [];
+    let adjustment = 1;
+    let endTimestamp = getEndTimestamp();
+    
+    // If the date in the query string is before 1/1/2021, use populations for July 2020.
+    // Otherwise, use populations for July 2021.
+    if (endTimestamp < 1609459200000) {
+        adjustment = censusData.adj2020;
+    }
+    else {
+        adjustment = censusData.adj2021;
+    }
+
     states.forEach(state => {
         let stateName = state.name;
         let stateAbbr = state.abbr;
@@ -108,11 +172,13 @@ function fillMap() {
         let url = `https://api.covidactnow.org/v2/state/${stateAbbr}.timeseries.json?apiKey=${apiKey}`;
         axios.get(url)
             .then(response => {
-                let days = response.data.actualsTimeseries
-                let deaths = getDeathCount(days);
-                let ddpm = parseFloat(1000000) * deaths / 14 / statePop;
+                let dataSeries = response.data.actualsTimeseries;
+                let deathCount = getDeathCount(dataSeries, endTimestamp);
+                let deaths = deathCount.deaths;
+                let nullDays = deathCount.nullDays;
+                let ddpm = parseFloat(1000000) * deaths / 14 / (statePop * parseFloat(adjustment));
                 let color = getColor(ddpm);
-                let tooltip = getTooltip(stateName, deaths, ddpm);
+                let tooltip = getTooltip(stateName, deaths, ddpm, nullDays);
                 setColorAndTooltip(stateAbbr, color, tooltip);
             })
             .catch(error => {
